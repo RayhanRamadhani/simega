a7d7ae7ca8eb0d80f7defa4d659a98f35f859669
Rayhan Ramadhani <58447376+RayhanRamadhani@users.noreply.github.com>
2025-03-07 18:56:55 +0700

    Chatbot

---
+++ app/Http/Controllers/ChatbotController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Throwable;
+
+class ChatbotController extends Controller
+{
+    public function __invoke(Request $request): string
+    {
+        try {
+            $systemPrompt = "kamu seorang project manager yang membantu user merancang sebuah rancangan list list tugas yang akan dibuat untuk aplikasi to-do list simega dari ide user\n\nUser: ";
+            
+            $responseAsString = Http::withHeaders([
+                "Content-Type" => "application/json",
+                "x-goog-api-key" => env('GEMINI_API_KEY')
+            ])->post('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro-exp-02-05:generateContent', [
+                "contents" => [
+                    [
+                        "parts"=> [
+                            ["text" => $systemPrompt . $request->post('content')]
+                        ]
+                    ]
+                ],
+                "generationConfig" => [
+                    "temperature" => 0.7,
+                    "maxOutputTokens" => 500,
+                ]
+            ])->body();
+
+            $responseData = json_decode($responseAsString, true);
+
+            if (isset($responseData['error'])) {
+                return "Gemini error: " . $responseData['error']['message'];
+            }
+
+            if (isset($responseData['candidates'][0]['content']['parts'][0]['text'])) {
+                return $responseData['candidates'][0]['content']['parts'][0]['text'];
+            }
+            return "Error: incomplete response";
+        } catch (Throwable $e) {
+            return $e->getMessage();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/chatbot', function () {
+    return view('chatbot');
+});
+
+Route::post('/chatbot', 'App\Http\Controllers\ChatbotController');
