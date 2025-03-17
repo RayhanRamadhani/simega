628058b1bf3fcef4129b2cae535ce353da06185b
Nrazb <nauraazzahra457@gmail.com>
2025-03-17 11:49:29 +0700

    Dashboard
    nyicil buat dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,16 @@
-<h1>Halo! ini dashboard </h1>
-<a href="/chatbot"><button>chat bot</button></a>
-<a href="/logout"><button>logout</button></a>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="flex justify-left items-center">
+        <img src="{{ asset('images/roki.png') }}" alt="Maskot" class="w-32 h-30">
+        <div class="ml-4 text-left">
+            <h1 class="text-5xl font-bold">Hai,</h1>
+            <h2 class="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-500">ADMIN!</h2>
+            <p class="text-gray-600 text-lg">Mau Nugas Apa Hari Ini?</p>
+        </div>
+    </div>
+    <a href="/chatbot"><button>chat bot</button></a>
+    <a href="/logout"><button>logout</button></a>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <div class="flex items-center">
             <button class="text-2xl mr-4">&#9776;</button> <!-- Hamburger Menu -->
             <span class="font-bold text-lg">SIMEGA</span>
+
+            <a href="/logout" class="border border-blue-500 text-blue-500 px-4 py-2 rounded-full hover:bg-blue-500 hover:text-white transition">Logout</a>
         </div>
     </nav>
 
