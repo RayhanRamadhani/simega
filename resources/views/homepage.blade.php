a9a396d1c6c17ac36f5178143e09f3df4ea875ab
PikunEz <fekadava@gmail.com>
2025-05-16 10:59:05 +0700

    Update Hompage
    sedikit pengubahan deskripsi

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -26,27 +26,25 @@
                 </div>
 
                 <div class="flex justify-center items-center text-left mt-6">
-                    <p class="text-gray-700 max-w-lg">
-                        SiMega adalah solusi modern untuk mengelola tugas dan aktivitas harian dengan lebih mudah dan terstruktur.
-                        Tersedia untuk web dan Android, aplikasi ini dirancang untuk membantu Anda tetap produktif, fokus,
-                        dan mencapai target tanpa hambatan.
+                    <p class="text-gray-700 max-w-lg text-justify">
+                        SiMega adalah solusi modern untuk mengelola tugas dan aktivitas harian secara lebih mudah dan terstruktur. Tersedia di platform web dan Android, aplikasi ini dirancang untuk membantu Anda tetap produktif, fokus, dan mencapai target tanpa hambatan.
                     </p>
-                    <img src="{{ asset('images/roki.png') }}" alt="Maskot" class="w-32 h-30">
+                    <img src="{{ asset('images/roki.png') }}" alt="Maskot" class="w-32 h-30 ml-4">
                 </div>
 
                 <h3 class="text-2xl font-bold mt-10">Fitur Unggulan SiMega</h3>
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center mt-10 max-w-4xl mx-auto">
                     <div class="bg-white shadow-lg rounded-xl p-6">
                         <h3 class="text-lg font-bold">Chat Bot</h3>
-                        <p class="text-gray-600">Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas rutrum donec euismod euismod imperdiet sollicitudin. Tempus egestas augue, ad class phasellus ex laoreet.<p>
+                        <p class="text-gray-600 text-justify">Roki, chatbot yang siap membantu Anda menyelesaikan tugas dan keperluan Anda dengan mudah.<p>
                     </div>
                     <div class="bg-white shadow-lg rounded-xl p-6">
                         <h3 class="text-lg font-bold">Anti Distract</h3>
-                        <p class="text-gray-600">Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas rutrum donec euismod euismod imperdiet sollicitudin. Tempus egestas augue, ad class phasellus ex laoreet.</p>
+                        <p class="text-gray-600 text-justify">Nikmati fitur andalan kami yang dirancang khusus untuk Anda yang ingin fokus tanpa gangguan. (Tersedia eksklusif di perangkat mobile)</p>
                     </div>
                     <div class="bg-white shadow-lg rounded-xl p-6">
                         <h3 class="text-lg font-bold">Statistik</h3>
-                        <p class="text-gray-600">Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas rutrum donec euismod euismod imperdiet sollicitudin. Tempus egestas augue, ad class phasellus ex laoreet.</p>
+                        <p class="text-gray-600 text-justify">Fitur statistik menyajikan data kuantitatif dari tugas yang Anda selesaikan dalam bentuk grafik yang informatif.</p>
                     </div>
                 </div>
             </div>
